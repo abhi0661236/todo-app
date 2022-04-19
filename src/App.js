3faf68939808d219aa0abcc39b7605ee61215013
abhi0661236 <ap0661236@gmail.com>
2022-04-19 22:57:19 +0530

    uploading a new project

--- src/App.js
+++ src/App.js
@@ -1,25 +1,56 @@
-import logo from './logo.svg';
+
 import './App.css';
 
 function App() {
+
+  const todos = [
+    { id: 1, text: "Wash dishes", done: false },
+    { id: 2, text: "Do laundry", done: false },
+    { id: 3, text: "Take shower", done: false }
+  ];
+
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <h1 className='font-bold text-xl text-grey-darkest'>Todo List</h1>
+      <hr />
+      <TodoList todos={todos} />
+      <AddTodo />
     </div>
   );
 }
+
+
+function TodoList({todos}) {
+  // console.log(props) // {todos: Array(3)}
+  return (
+    <div className='todos-container'>
+      <ul>
+      { 
+        todos.map(todo => (<li key = { todo.id  } >{ todo.text  }</li>)
+      ) 
+    }      
+    </ul>
+    </div>
+    
+  );
+}
+
+function AddTodo(){
+
+  function handleAddTodo(event) {
+    console.log(event);
+    event.preventDefault();
+  }
+
+  return (
+    <form className='add-form' onSubmit={handleAddTodo}>
+      <input className='add-input' placeholder="Add todo" />
+      <button className='submit-btn' type="submit">Submit</button>
+    </form>
+  );
+}
+
+
+
 
 export default App;
