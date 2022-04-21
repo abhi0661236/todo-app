b4e69e9d49cf907d0f347f1142a1372fc179d8cb
abhi0661236 <ap0661236@gmail.com>
2022-04-21 20:30:10 +0530

    added the delete functionality so the user can delete the todo item

--- src/App.js
+++ src/App.js
@@ -5,7 +5,7 @@ function App() {
 
   const [todos, setTodos] = React.useState([
     { id: 1, text: "Wash dishes", done: false },
-    { id: 2, text: "Do laundry", done: false },
+    { id: 2, text: "Do laundry", done: true },
     { id: 3, text: "Take shower", done: false }
   ]);
 
@@ -15,27 +15,55 @@ function App() {
     <div className="App">
       <h1 className='font-bold text-xl text-grey-darkest'>Todo List</h1>
       <hr />
-      <TodoList todos={todos} />
+      <TodoList todos={todos} setTodos ={setTodos}/>
       <AddTodo setTodos ={setTodos} />
     </div>
   );
 }
 
 
-function TodoList({todos}) {
+function TodoList({todos, setTodos}) {
   // console.log(props) // {todos: Array(3)}
+  function handleToggleTodo(todo) {
+
+
+    const updatedTodos = todos.map((t) =>
+      t.id === todo.id
+        ? {
+            ...t,
+            done: !t.done
+          }
+        : t
+    );
+
+
+    setTodos(updatedTodos);
+
+    setTodos(updatedTodos);
+  }
   return (
     <div className='todos-container'>
       <ul>
-      { 
-        todos.map(todo => (<li key = { todo.id  } >{ todo.text  }</li>)
-      ) 
-    }      
+      {todos.map((todo) => (
+        <li
+          onDoubleClick={() => handleToggleTodo(todo)}
+          style={{
+            textDecoration: todo.done ? "line-through" : ""
+          }}
+          key={todo.id}
+        >
+          {todo.text}
+          <DeleteTodo todo={todo} setTodos={setTodos} />
+        </li>
+      ))}
     </ul>
     </div>
     
   );
 }
+
+
+
 
 function AddTodo({setTodos}){
 
@@ -68,7 +96,31 @@ function AddTodo({setTodos}){
   );
 }
 
+function DeleteTodo({ todo, setTodos }) {
+  function handleDeleteTodo() {
+    const confirmed = window.confirm("Do you want to delete this?");
+    if (confirmed) {
+      setTodos((prevTodos) => {
+        return prevTodos.filter((t) => t.id !== todo.id);
+      });
+    }
+  }
 
+  return (
+    <span
+      onClick={handleDeleteTodo}
+      role="button"
+      style={{
+        color: "red",
+        fontWeight: "bold",
+        marginLeft: 10,
+        cursor: "pointer"
+      }}
+    >
+      x
+    </span>
+  );
+}
 
 
 export default App;
